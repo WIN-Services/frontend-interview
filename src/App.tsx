80ce6e150013ac10e37f3f60edd2d6528b11455e
Maanil Verma <maanilv8@gmail.com>
2022-11-11 21:51:14 +0530

    Integrate Components & Remove Redundant Files

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 import React from "react";
-import "./App.css";
+
+import { ContactForm } from "./components/ContactForm";
+import styles from "./App.module.scss";
+import { EmailSubscription } from "./components/EmailSubscription";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <ContactForm />
+      <EmailSubscription />
     </div>
   );
 }
