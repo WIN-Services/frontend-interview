726fed59233a5150d2db64495c3b25e5da6cc6b3
shivamdubey8585 <96253645+shivamdubey8585@users.noreply.github.com>
2022-12-04 03:21:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Contact from './components/Contact';
+import Subscribe from './components/Subscribe';
+
+function App() {
+  return (
+    <div className="App">
+      <Contact />
+      <Subscribe />
+    </div>
+  );
+}
+
+export default App;
