8af44524e3dee848c51a92b89f62eb25ab061c25
Vasu Singh <74423511+vasu181100@users.noreply.github.com>
2022-12-03 16:45:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import Contacthp from './components/Contacthp';
+import Label from './components/Label';
+import Textbox from './components/Textbox';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <div className="box">
+        <div className="contact">
+          <p className='h4'>Contact Us</p>
+          <Contacthp title="Inspections" info="(800) 309-6753 | info@wini.com " />
+          <Contacthp title="Franchising" info="(800) 309-6753 | info@wini.com " />
+          <Contacthp title="Vendors" info="(312) 557-9319 | vendors@wini.com" />
+        </div>
+        <div className="form">
+          <div className="top">
+            <div className="element">
+              <Label title="Name" />
+              <Textbox ofType={"text"} st="normal" />
+            </div>
+            <div className="element">
+              <Label title="Name" />
+              <Textbox ofType={"text"} st="normal" />
+            </div>
+          </div>
+          <div className="down">
+            <div className="element">
+              <Label title="Name" />
+              <Textbox ofType={"text"} st="normal" />
+            </div>
+            <div className="element">
+              <Label title="Name" />
+              <Textbox ofType={"text"} st="normal" />
+            </div>
+          </div>
+          <div className="element">
+              <Label title="Name" />
+              {/* <Textbox ofType={"text"} st="area" /> */}
+              <textarea name="" id="" cols="30" rows="10" className='area input-text' />
+              <Button ofText="Submit" btnstyle="submit" />
+          </div>
+            
+        </div>
+      </div>
+      <footer>
+        <p className='h3'>Subscribe to our emails </p>
+        <p className='med-reg'>Enter your email address to receive the latest notifications, newsletters, and updates from WIN! </p>
+        <div className="sub">
+          <Textbox />
+          <Button title="Subscribe" btnstyle={"subscribe"} />
+        </div>
+      </footer>
+      {/* <h1>Hello World</h1>
+      <Label text="Name" />
+      <Textbox ofType={"text"} />
+      <Button ofText="Submit" style="" /> */}
     </div>
   );
 }
