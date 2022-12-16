d98411e4ba4fc2a3785d2cd650781e3442941a42
TwinkleJaiswal <twinklejaiswal8485@gmail.com>
2022-12-16 18:50:28 +0530

    intial first

---
+++ src/contactdetail.js
@@ -0,0 +1,14 @@
+
+const contactdetail = {
+    YOUR_FONE: '(800) 309-6753',
+    Your_Phone: '(312) 557-9319',
+    User_Email: 'info@wini.com',
+    Vendor_Email: 'vednor@wini.com',
+
+};
+
+
+export {
+
+    contactdetail
+}
