70625db4bdb670f3374638136ade5ec02c856e66
TwinkleJaiswal <twinklejaiswal8485@gmail.com>
2022-12-16 18:24:05 +0530

    intial commit

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
+import ContactUs from '../contact/index'
+
+const App=()=>{
+    return(
+        <>
+         <ContactUs/>
+         </>
+    )
+}
+export default App
