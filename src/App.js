8964203e70a808639d3b16ef2342b2a01e1ba5b6
Atul <atulreso1@gmail.com>
2022-12-21 12:14:45 +0530

    Feat: replicate figma design with code and add required components with style libraries

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import Contact from "./components/Contact";
+
 import './App.css';
+
+const theme = createTheme({
+  typography: {
+    h4: {
+      color: "#004461",
+    },
+    subtitle2: {
+      color: "#004461",
+    },
+    h1: {
+      color: "#004461",
+    },
+    overline: {
+      color: "#2f2f2f",
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <ThemeProvider theme={ theme }>
+        <Container maxWidth="lg">
+          <Contact />
+        </Container>
+      </ThemeProvider>
     </div>
   );
 }
