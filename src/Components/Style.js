c514b3806aec6d408abb88342298a403f44b6191
Sahil Chikara <sahilchikara015@gmail.com>
2023-05-21 00:01:06 +0530

    Assignment Submission

---
+++ src/Components/Style.js
@@ -0,0 +1,87 @@
+export const rightInputStyle={
+    height:"66px",
+    fontSize: "15px",
+    py:2,
+    pl:4,
+    mr:2.5,
+    bgcolor: "#FFFFFF",
+    border:1,
+    borderColor: "#EFF0F7",
+    boxShadow: "0px 2px 6px rgba(19, 18, 66, 0.07)",
+    borderRadius: 2.5,
+} 
+export const leftInputStyle={
+    height:"66px",
+    fontSize: "15px",
+    py:2,
+    pl:4,
+    mr:2.5,
+    bgcolor: "#FFFFFF",
+    border:1,
+    borderColor: "#EFF0F7",
+    boxShadow: "0px 2px 6px rgba(19, 18, 66, 0.07)",
+    borderRadius: 2.5,
+} 
+
+export const messageInputStyle={
+    fontSize: "15px",
+    py:2,
+    pl:4,
+    mr:2.5,
+    bgcolor: "#FFFFFF",
+    border:1,
+    borderColor: "#EFF0F7",
+    boxShadow: "0px 2px 6px rgba(19, 18, 66, 0.07)",
+    borderRadius: 2.5,
+} 
+
+export const sumbitButtonStyle = {
+  width: "129px",
+  height: "58px",
+  background: "#005981",
+  borderRadius: "10px",
+  fontFamily: "Axiforma",
+  fontStyle: "normal",
+  fontWeight: "700",
+  fontSize: "16px",
+  textAlign: "center",
+  letterSpacing: "0.46px",
+  textTransform: "uppercase",
+  color: "#FFFFFF",
+};
+
+export const leftContainerHeading={
+    fontStyle: "normal",fontWeight: "800",fontSize: "25px",marginBottom:32,fontFamily:"Axiforma",color:"#000000"
+}
+
+export const leftContainerSubHeading={
+    fontStyle: "normal",fontWeight: "600",fontSize: "16px",marginBottom:32,fontFamily:"Axiforma",color:"#000000"
+}
+
+export const leftContainerText={
+    fontStyle: "normal",fontWeight: "600",fontSize: "12px",marginBottom:32,fontFamily:"Rubik",color:"#000000"
+}
+
+export const bottomContainerHeading={
+    fontStyle: "normal",fontWeight: "800",fontSize: "25px",marginBottom:32,fontFamily:"Axiforma"
+}
+
+export const labelText={
+    fontStyle: "normal",fontWeight: "500",fontSize: "18px",marginBottom:8,fontFamily:"Axiforma",color:"#000000"
+}
+
+export const subscibeBtn = {
+  marginTop: 36,
+  marginBottom: 16,
+  backgroundColor: "#D4A656",
+  borderRadius: "10px",
+  height: "60px",
+  width: "209px",
+  border: "2px solid #D4A656",
+  fontFamily:"OpenSans",
+  fontStyle:"normal",
+  fontWeight:"700",
+  fontSize:"20px",
+  color:"#FFFFFF",
+  textTransform:"none"
+};
