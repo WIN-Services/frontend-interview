e2bd9de4210066317fc5644ef56dbe7c3c679697
Ayush <ayush.yadav@intellemo.com>
2023-07-02 03:19:18 +0530

    added state  to the form

--- src/pages/contactus/index.js
+++ src/pages/contactus/index.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contactus.css";
 import { TextField } from "@mui/material";
 import ContactusFooter from "../../components/footer/ContactusFooter";
 
 function ContactUs() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    zip: "",
+    message: "",
+  });
+
   const data = [
     { title: "Inspections", detail: "(800) 309-6753 | info@wini.com " },
     { title: "Franchising", detail: "(800) 309-6753 | info@wini.com " },
@@ -11,11 +19,31 @@ function ContactUs() {
   ];
 
   const inputFields = [
-    { label: "Name", placeholder: "Full Name", type: "text" },
-    { label: "Email", placeholder: "xyz@abc.com", type: "email" },
-    { label: "Phone Number", placeholder: "(123) 456-7890", type: "number" },
-    { label: "ZIP Code", placeholder: "XXXX", type: "number" },
+    { label: "Name", placeholder: "Full Name", type: "text", name: "name" },
+    {
+      label: "Email",
+      placeholder: "xyz@abc.com",
+      type: "email",
+      name: "email",
+    },
+    {
+      label: "Phone Number",
+      placeholder: "(123) 456-7890",
+      type: "number",
+      name: "phone",
+    },
+    { label: "ZIP Code", placeholder: "XXXX", type: "number", name: "zip" },
   ];
+
+  const handleFormInput = (e) => {
+    const { name, value } = e.target;
+    const sanitizedValue = value.replace(/\n/g, "");
+    setFormData({ ...formData, [name]: sanitizedValue });
+  };
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
 
   return (
     <div className="contactus">
@@ -30,7 +58,7 @@ function ContactUs() {
               </div>
             ))}
         </div>
-        <div style={{ width: "100%" }}>
+        <form style={{ width: "100%" }} onSubmit={(e) => handleFormSubmit(e)}>
           <div className="contactForm">
             {inputFields.map((field, i) => (
               <div key={i}>
@@ -38,8 +66,11 @@ function ContactUs() {
                 <TextField
                   placeholder={field.placeholder}
                   type={field.type}
+                  name={field.name}
                   variant="outlined"
+                  onChange={(e) => handleFormInput(e)}
                   className="customTextField"
+                  required
                 />
               </div>
             ))}
@@ -49,15 +80,20 @@ function ContactUs() {
             <TextField
               placeholder={"Please type it in here..."}
               type={"text"}
+              name="message"
               variant="outlined"
-              multiline // Enable multiline input
+              multiline
               rows={5}
               className="customTextField"
+              onChange={(e) => handleFormInput(e)}
               fullWidth
+              required
             />
           </div>
-          <button className="submitBtn">Submit</button>
-        </div>
+          <button className="submitBtn" type="submit">
+            Submit
+          </button>
+        </form>
       </div>
       <div className="contactusFooter">
         <ContactusFooter />
