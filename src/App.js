105bd1c4264ad8332156077301af23eda2584322
v.anhad-at-632228229419 <vanhad72@gmail.com>
2023-07-03 15:16:24 +0530

    added contact us form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ContactUs from './pages/ContactUs';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<ContactUs/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
