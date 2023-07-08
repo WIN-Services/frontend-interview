9a2c54d76eb52c7f8ed9de259f0e8e4925f23ab0
Anchal Tyagi <anchaltyagi000@gmail.com>
2023-07-08 12:49:48 +0530

    Remove unused code and refactored string usage

---
+++ src/Constant/Strings/index.js
@@ -0,0 +1,13 @@
+export const Strings = {
+  name_err_msg: "Please enter valid name",
+  email_err_msg: "Please enter valid email id",
+  mobile_err_msg: "Please enter valid phone number",
+  zipcode_err_msg: "Please enter valid zip code",
+  msg_err: "Please enter valid message",
+
+  subscribe_heading: "Subscribe to our emails",
+  subscribe_msg:
+    "Enter your email address to receive the latest notifications, newsletters, and updates from WIN!",
+  form_success_msg: "You have successfully submitted",
+  subscribe_success_msg: "You have successfully subscribed",
+};
