632a8947d5b49cc909526ee8424a95667be82b01
Mandeep Singh <mandeep1154@gmail.com>
2023-07-12 14:39:07 +0530

    mandeep1012@hotmail.com assignment submission

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import ContactForm from './ContactUs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <ContactForm />
     </div>
   );
 }
